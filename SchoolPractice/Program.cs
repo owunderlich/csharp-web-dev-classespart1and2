3d3b47e9beb42798f71d9a4b775a0d227cd18e28
Oliver Wunderlich <oliver.wunderlich@jostchemical.com>
2021-11-11 16:02:31 -0600

    4.7 Exercises

--- SchoolPractice/Program.cs
+++ SchoolPractice/Program.cs
@@ -7,6 +7,15 @@ namespace SchoolPractice
         public static void Main(string[] args)
         {
             // TODO: Instantiate your objects and test your exercise solutions with print statements here.
+            Student me = new Student();
+            me.Name = "Yolo Swaggins";
+            me.NumberOfCredits = 100;
+            me.Gpa = 4.5;
+            me.StudentId = 0;
+            Console.WriteLine($"Name: {me.Name}\n" +
+                              $"Student ID: {me.StudentId}\n" +
+                              $"Credits: {me.NumberOfCredits}\n" +
+                              $"GPA: {me.Gpa}");
         }
     }
 }
