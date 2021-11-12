fb1c028da4e740015abc54f8732e14ce26ae5862
OJTang <oliverwunderlich13@gmail.com>
2021-11-12 00:21:48 -0600

    practice

---
+++ SchoolPractice/Course.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SchoolPractice
+{
+	public class Course
+	{
+		public string Name { get; set; }
+		public List<Teacher> Teachers { get; set; }
+		public List<Student> Students { get; set; }
+	}
+}
