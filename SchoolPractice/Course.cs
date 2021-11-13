5f9c56716eea2a23e58bb954fa5a43afe94c3f24
Oliver Wunderlich <oliver.wunderlich@jostchemical.com>
2021-11-12 18:50:03 -0600

    waafsfd

--- SchoolPractice/Course.cs
+++ SchoolPractice/Course.cs
@@ -6,7 +6,19 @@ namespace SchoolPractice
 	public class Course
 	{
 		public string Name { get; set; }
-		public List<Teacher> Teachers { get; set; }
+		public Teacher Instructor { get; set; }
 		public List<Student> Students { get; set; }
-	}
+
+        public override bool Equals(object obj)
+        {
+            return obj is Course course &&
+                   Name == course.Name &&
+                   EqualityComparer<Teacher>.Default.Equals(Instructor, course.Instructor);
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Name, Instructor);
+        }
+    }
 }
